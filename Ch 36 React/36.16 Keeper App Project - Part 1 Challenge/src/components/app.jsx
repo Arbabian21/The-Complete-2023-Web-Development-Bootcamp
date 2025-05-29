b9c4e5653dba69ac33a674af8df73d2821ac336d
Arbab <arifarbab21@gmail.com>
2025-05-29 16:25:16 -0400

    keeper project part 1

---
+++ Ch 36 React/36.16 Keeper App Project - Part 1 Challenge/src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Note />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
---
+++ Ch 36 React/36.16 Keeper App Project - Part 1 Challenge/src/components/note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+	return (
+		<div className={'note'}>
+			<h1>note header</h1>
+			<p>note</p>
+		</div>
+	);
+}
+
+export default Note;
