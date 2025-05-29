14d35f86f7d2c80fca3b00571cfd35bdfc0c57bc
Arbab <arifarbab21@gmail.com>
2025-05-29 14:24:21 -0400

    36.11 and 36.12 styling and components respectively

---
+++ Ch 36 React/36.12 React Components/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import List from "./List";
+
+export function App() {
+  return (
+    <>
+      <Heading />
+      <List />
+    </>
+  );
+}
+
+export default App;
